21564d5ecd84f47e77355b4a7b298c01541934b0
Steven.Mejia <Steven.Mejia@gmail.com>
2012-06-07 02:38:45 +0000

    Hasta aqui trabaje en la monitoria el 6 de junio del 2012 siendo las  9:38 pm

--- 8Puzzle/src/v0/Solver.java
+++ 8Puzzle/src/v0/Solver.java
@@ -8,10 +8,38 @@ import java.util.Scanner;
 
 public class Solver {
 
+	public Solver(Board initial)
+	{
+		
+	}
+	
+	public boolean isSolvable()
+	{
+		return false;
+	}
+
+	public int moves()
+	{
+		return 0;
+	}
+	
+	public String toString()
+	{
+		return null;
+	}
+
+	
+	
 	/**
+	 * Main del programa
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		
+		/************************************************************************
+		 * INICIA CAPTURA DESDE LA ENTRADA ESTANDAR PARA CONSTRUIR EL TABLERO.
+		 * 
+		 ************************************************************************/
 		Scanner sc = new Scanner(System.in);
 		int size = Integer.parseInt(sc.nextLine());
 		int[][] board = new int[size][size];
@@ -24,8 +52,9 @@ public class Solver {
 		}
 		
 		Board bd = new Board(board);
+		System.out.print("Valor Manhatan: "+bd.manhattan()+"\nValor Hamming: "+bd.hamming());
 		//bd.hamming();
-		System.out.print(bd.toString());
+		//System.out.print(bd.toString());
 		
 	}
 
